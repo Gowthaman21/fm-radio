075fee4cf2a7d49b28b4dbaac4ae8b2d3af7032c
Gowthaman21 <manogowtham211@gmail.com>
2021-12-19 21:25:44 +0530

    clear cache

--- src/theme.js
+++
@@ -1,5 +0,0 @@
-export const DARK_BG = "#253344";
-export const DARK_BG2 = "#3A506B";
-export const DARK_ICON = "#6FFFE9";
-export const FOOTER_BG = "#1C2541";
-export const FOOTER_TEXT = "#5BC0BE";
