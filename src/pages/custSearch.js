6985dd95b4a6ae04dc40121d7dcf9000ddd01f75
Gowthaman21 <manogowtham211@gmail.com>
2021-12-19 17:31:39 +0530

    Added footer

--- src/pages/custSearch.js
+++ src/pages/custSearch.js
@@ -5,9 +5,13 @@ import {
     VStack,
     Center,
     Button,
+    Box,
+    HStack,
 } from "native-base";
 import React, { useState, useContext } from "react";
 import { AudioContext } from "../context/AudioProvider";
+import { DARK_BG, DARK_BG2, DARK_ICON } from "../theme";
+import Footer from "./footer";
 
 const CustomSearch = ({ navigation }) => {
     const [search, setSearch] = useState({
@@ -15,64 +19,75 @@ const CustomSearch = ({ navigation }) => {
         language: "",
         genre: "",
     });
-    const { sta } = useContext(AudioContext);
+    const { filterStations, details } = useContext(AudioContext);
     const handleChange = async (newState) => {
         setSearch((prevState) => ({
             ...prevState,
             ...newState,
         }));
     };
+    function isEmpty(obj) {
+        var i = Object.keys(obj).length !== 0;
+        return i;
+    }
     return (
         <NativeBaseProvider>
-            <Center flex={1}>
-                <VStack width="90%" mx="3">
-                    <FormControl>
-                        <FormControl.Label _text={{ bold: true }}>
-                            Country Code
-                        </FormControl.Label>
-                        <Input
-                            placeholder="US, IN"
-                            onChangeText={(value) => {
-                                handleChange({ countryCode: value });
+            <Box safeArea flex={1} bg={DARK_BG}>
+                <Center flex={1}>
+                    <VStack width="90%" mx="3">
+                        <FormControl>
+                            <FormControl.Label _text={{ bold: true }}>
+                                Country Code
+                            </FormControl.Label>
+                            <Input
+                                placeholder="US, IN"
+                                onChangeText={(value) => {
+                                    handleChange({ countryCode: value });
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label _text={{ bold: true }}>
+                                Language
+                            </FormControl.Label>
+                            <Input
+                                placeholder="tamil"
+                                onChangeText={(value) => {
+                                    handleChange({ language: value });
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label _text={{ bold: true }}>
+                                Genre
+                            </FormControl.Label>
+                            <Input
+                                placeholder="jazz"
+                                onChangeText={(value) => {
+                                    handleChange({ genre: value });
+                                }}
+                            />
+                        </FormControl>
+                        <Button
+                            onPress={async () => {
+                                let d = await filterStations(search);
+                                if (d) {
+                                    navigation.navigate("station");
+                                }
                             }}
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label _text={{ bold: true }}>
-                            Language
-                        </FormControl.Label>
-                        <Input
-                            placeholder="tamil"
-                            onChangeText={(value) => {
-                                handleChange({ language: value });
-                            }}
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label _text={{ bold: true }}>
-                            Genre
-                        </FormControl.Label>
-                        <Input
-                            placeholder="jazz"
-                            onChangeText={(value) => {
-                                handleChange({ genre: value });
-                            }}
-                        />
-                    </FormControl>
-                    <Button
-                        onPress={async () => {
-                            let d = await sta(search);
-                            if (d) {
-                                navigation.navigate("station");
-                            }
-                        }}
-                        mt="5"
-                        colorScheme="cyan"
-                    >
-                        Submit
-                    </Button>
-                </VStack>
-            </Center>
+                            mt="5"
+                            colorScheme="cyan"
+                        >
+                            Submit
+                        </Button>
+                    </VStack>
+                </Center>
+                {isEmpty(details.currentStation) && (
+                    <HStack>
+                        <Footer />
+                    </HStack>
+                )}
+            </Box>
         </NativeBaseProvider>
     );
 };
